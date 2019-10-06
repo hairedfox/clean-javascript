ba8daa0b6bcdfcffef94446b58a62c96c87f178b
Hai Tran <hai.redfox@outlook.com>
2019-10-07 02:37:00 +0700

    Refactor code

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
+const validEmail = /\S+@\S+\.\S+/
+
+const validUser = ({email, phone}) => {
+  return email && validEmail.test(email) && phone
+}
+
+const multiply = (base) => {
+  return (val) => Number(val) * base
+}
+
+const multiplyBy12 = multiply(12)
+
+const generateRandomNumber = () => {
+  return Math.floor((Math.random() * 100) + 1)
+}
+
+const enoughIncome = (annualIncome, hasA) => {
+  return annualIncome >= 100000 && hasA
+}
+
+const unpredictableError = () => {
+  return new Error('Something went wrong')
+}
+
 const saveUser = (user, db) => {
-  if (user.email && /\S+@\S+\.\S+/.test(user.email) && user.phone) {
-    let hasA = false;
-    user.permissions.forEach(permission => {
-      if (permission === "A") {
-        hasA = true;
-      }
-    });
-    const annualIncome = Number(user.monthlyIncome) * 12;
-    if (annualIncome >= 100000 && hasA) {
-      try {
-        user.id = Math.floor((Math.random() * 100) + 1); // generate a random id between 1 and 100 (don't do it in production :P)
-        db.init();
-        db.save(user);
-      } catch (e) {
-        throw new Error("Something went wrong");
-      }
-    } else {
-      throw new Error("No enough annual income");
+  if (!validUser(user))
+    throw unpredictableError()
+  let hasA = false;
+  user.permissions.forEach(permission => hasA = permission === 'A')
+  const annualIncome = multiplyBy12(user.monthlyIncome);
+  if (enoughIncome(annualIncome, hasA)) {
+    try {
+      user.id = generateRandomNumber() // generate a random id between 1 and 100 (don't do it in production :P)
+      db.init();
+      db.save(user);
+    } catch (e) {
+      throw unpredictableError()
     }
   } else {
-    throw new Error("Something went wrong");
+    throw new Error("No enough annual income");
   }
 };
 
